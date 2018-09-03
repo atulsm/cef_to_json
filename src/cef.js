33cd56dbd319631b2b9ce21342c93cf0eb20ddef
Atul Soman <atul.soman@microfocus.com>
2018-09-03 13:53:36 +0530

    Implementing cef parsing with sample testcase

--- src/cef.js
+++ src/cef.js
@@ -1,6 +1,55 @@
 
-function toJson(cef) {
-  return cef;
+/**
+ * Converts CEF sring to json
+ * @param {string} cefOrig The original String which is passed
+ * @returns {JSON} Parsed CEF string
+ */
+function toJson(cefOrig) {
+  const cefVerPos = cefOrig.indexOf('CEF:');
+  if (cefVerPos < 0) {
+    console.log('Cannot find CEF version. Probably not a vliad CEF string');
+    return undefined;
+  }
+
+  const cef = cefOrig.substring(cefVerPos).trim();
+  const ret = {};
+
+  ret.cefversion = cef.charAt(4);
+  const headers = cef.split('|');
+  if (headers.length < 7) {
+    console.log('Cannot find a valid CEF header.');
+    return undefined;
+  }
+
+  // extracting headers
+  [ret.vendor, ret.product, ret.deviceversion, ret.classid, ret.name, ret.severity] = headers
+    .slice(1, 7);
+
+  if (headers.length === 7) {
+    return ret;
+  }
+
+  // extracting extension fields which are in key=value format
+  const re = /(\w*)=/g;
+  const str = headers[7];
+  const exts = [];
+  /*eslint-disable*/
+  while ((m = re.exec(str)) !== null) {
+    exts.push({key:m[1], pos:m.index}); 
+  }
+  /* eslint-enable */
+
+  exts.forEach((ext, index) => {
+    let nextPos;
+    if (index === (exts.length - 1)) {
+      nextPos = str.length;
+    } else {
+      nextPos = exts[index + 1].pos;
+    }
+    ret[ext.key] = str.substring(ext.pos + ext.key.length + 1, nextPos).trim();
+  });
+
+  return ret;
 }
 
 module.exports = {
--- test/cef.test.js
+++ test/cef.test.js
@@ -1,5 +1,18 @@
 const cef = require('../src/cef.js');
 
 test('cef 0.1 header check', () => {
-  expect(cef.toJson("")).toEqual("");
+  expect(cef
+    .toJson('CEF:0|Security|threatmanager2|1.0|100|worm successfully stopped|10|src=10.0.0.1 dst=2.1.2.2 spt=1232'))
+    .toEqual({
+      cefversion: '0',
+      vendor: 'Security',
+      product: 'threatmanager2',
+      deviceversion: '1.0',
+      classid: '100',
+      name: 'worm successfully stopped',
+      severity: '10',
+      src: '10.0.0.1',
+      dst: '2.1.2.2',
+      spt: '1232',
+    });
 });
